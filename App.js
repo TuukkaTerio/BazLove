e6b67cf4e48a3b6591d2591e20be18ec8ab1afd2
Tuukka Teriö <tuukka.terio@medieinstitutet.se>
2017-11-21 15:29:57 +0100

    Start

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, Button, View, AppRegistry, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    let pic = {
+      uri: 'https://pbs.twimg.com/profile_images/790944603373461504/VGq52TJ6_400x400.jpg'
+    };
+    return (
+      <View style={styles.container}>
+        <Text>Bazooka</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Button
+          onPress={() => this._handlePress()}
+          title="Send"
+        >
+          Send
+        </Button>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffd92a',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
