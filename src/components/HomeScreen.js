e4202e442429e4a481d5b1d6dbfd7b2b30d5a2ea
Tuukka Teriö <tuukka.terio@medieinstitutet.se>
2017-12-10 16:45:43 +0100

    Added user authorization with Firebase using password-based accounts.

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,34 +1,134 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, StyleSheet, View, Image, Text } from 'react-native';
+import * as firebase from 'firebase';
+import { Alert, TouchableOpacity, StyleSheet, View, Image, Text, TextInput } from 'react-native';
+import RenderIf from './RenderIf';
 import ButtonContent from './ButtonContent';
 
-const HomeScreen = ({ navigation }) => (
-  <View style={styles.HomeScreen}>
-    <Image
-      source={require('../img/baz.png')}
-      style={{width: 200, height: 200}}
-    />
-    <TouchableOpacity
-      onPress={() => navigation.navigate('Message')}
-      title='Send love'>
-      <ButtonContent
-        btnContent = {'Send love'}
-        btnColor = {'#49a38b'}
-      />
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress={() => navigation.navigate('ShowLove')}
-      title='Show love'>
-      <ButtonContent
-        btnContent = {'Show love'}
-        btnColor = {'#ffd92a'}
-      />
-    </TouchableOpacity>
-    <Text>Messages are deleted on Mondays at 11 a.m.</Text>
-  </View>
-);
+export default class HomeScreen extends React.Component {
 
-export default HomeScreen;
+  constructor(props) {
+    super(props);
+    this.state = {
+      userEmail: '',
+      userPassword: '',
+      screenContent: 'loading',
+      navigation: this.props.navigation,
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ screenContent: 'home' });
+      } else {
+        this.setState({ screenContent: 'login' });
+      }
+    });
+  }
+
+  // Handles the log in
+  handleLogin(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    // Log in successful.
+    .then(() => { this.setState({ screenContent: 'home' }); })
+    // An error happened.
+    .catch(() => {
+      this.setState({ screenContent: 'login' });
+      Alert.alert("User doesn't exist");
+      this.emailInput.clear();
+      this.passwordInput.clear();
+    });
+  }
+
+  // Handles the log out
+  handleLogout() {
+    firebase.auth().signOut().then(function() {
+      // Log out successful.
+    }).catch(function(error) {
+      // An error happened.
+      Alert.alert("Couldn't log out");
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.HomeScreen}>
+        {RenderIf(this.state.screenContent === 'home',
+          <View style={styles.HomeScreen}>
+            <Image
+              source={require('../img/baz.png')}
+              style={{width: 200, height: 200}}
+            />
+            <TouchableOpacity
+              onPress={() => this.state.navigation.navigate('Message')}
+              title='Send'>
+              <ButtonContent
+                btnContent = {'Send'}
+                btnColor = {'#49a38b'}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.state.navigation.navigate('ShowLove')}
+              title='Show'>
+              <ButtonContent
+                btnContent = {'Show'}
+                btnColor = {'#ffd92a'}
+              />
+            </TouchableOpacity>
+            <Text>Messages are deleted on Mondays at 11 a.m.</Text>
+            <TouchableOpacity
+              onPress={() => {this.handleLogout()}}
+              title='Logout'>
+              <ButtonContent
+                btnContent = {'Logout'}
+                btnColor = {'#49a38b'}
+              />
+            </TouchableOpacity>
+          </View>
+        )}
+        {RenderIf(this.state.screenContent === 'login',
+          <View style={styles.LoginScreen}>
+            <TextInput
+              style={styles.LoginInput}
+              ref={input => { this.emailInput = input }}
+              placeholder="Email"
+              onChangeText={(userEmail) => this.setState({userEmail})}
+              autoFocus = {true}
+              keyboardType = {'email-address'}
+              // Limits the maximum number of characters that can be entered.
+              maxLength = {300}
+              autoCapitalize = {'none'}
+              autoCorrect = {false}
+            />
+            <TextInput
+              style={styles.LoginInput}
+              ref={input => { this.passwordInput = input }}
+              placeholder="Password"
+              onChangeText={(userPassword) => this.setState({userPassword})}
+              // Limits the maximum number of characters that can be entered.
+              maxLength = {300}
+              autoCapitalize = {'none'}
+              autoCorrect = {false}
+              secureTextEntry = {true}
+            />
+            <TouchableOpacity
+              onPress={() => {this.handleLogin(this.state.userEmail, this.state.userPassword)}}
+              title='OK'>
+              <ButtonContent
+                btnContent = {'OK'}
+                btnColor = {'#49a38b'}
+              />
+            </TouchableOpacity>
+          </View>
+        )}
+        {RenderIf(this.state.screenContent === 'loading',
+          <View style={styles.HomeScreen}>
+          </View>
+        )}
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   HomeScreen: {
@@ -36,5 +136,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#331c48',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  LoginScreen: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  LoginInput: {
+    height: 50,
+    fontSize: 20,
   },
 });
