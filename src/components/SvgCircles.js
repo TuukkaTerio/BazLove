a985e7579f8723706b6e536a871dd3474e0b5bc8
Tuukka Teriö <tuukka.terio@medieinstitutet.se>
2017-12-17 14:09:32 +0100

    WIP: SVG background animation.

--- src/components/SvgCircles.js
+++ src/components/SvgCircles.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Animated,
+  Image,
+  Easing
+} from 'react-native';
 import Svg, { Circle,
     Ellipse,
     G,
@@ -16,50 +24,95 @@ import Svg, { Circle,
     Stop } from 'react-native-svg';
 
 export default class SvgCircles extends React.Component {
+
+  constructor () {
+    super()
+    this.spinValue = new Animated.Value(0)
+  }
+
+  componentDidMount () {
+    this.spin()
+  }
+  spin () {
+    this.spinValue.setValue(0)
+    Animated.timing(
+      this.spinValue,
+      {
+        toValue: 1,
+        duration: 3000,
+        easing: Easing.linear
+      }
+    ).start(() => this.spin())
+  }
+
   render(props) {
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
     const circleSize = this.props.circleSize;
     const circleColor = this.props.circleColor;
     const circleStyle = {
       height: circleSize,
       width: circleSize,
-      zIndex: -11,
+    }
+    const circlesContainer = {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
       position: 'absolute',
+      zIndex: -1,
+    }
+    const rotatingContainer = {
+      transform: [{rotate: spin}],
     }
     return (
-      <Svg viewBox={'0 0 300 300'} style={circleStyle}>
-        <Circle
-          cx="150"
-          cy="145"
-          r="100"
-          fill={circleColor}
-          // animation: from0to360 1s linear infinite;
-          // transform-origin: 145px 150px;
-        />
-        <Circle
-          cx="150"
-          cy="155"
-          r="100"
-          fill={circleColor}
-          // animation: from0to360 2s linear infinite;
-          // transform-origin: 155px 150px;
-        />
-        <Circle
-          cx="145"
-          cy="150"
-          r="100"
-          fill={circleColor}
-          // animation: from0to360 3s linear infinite;
-          // transform-origin: 150px 145px;
-        />
-        <Circle
-          cx="155"
-          cy="150"
-          r="100"
-          fill={circleColor}
-          // animation: from0to360 2.5s linear infinite;
-          // transform-origin: 150px 155px;
-        />
-      </Svg>
+      <View style={circlesContainer}>
+        <Animated.View style={rotatingContainer}>
+          <Svg viewBox={'0 0 300 300'} style={circleStyle}>
+            <Circle
+              cx="150"
+              cy="145"
+              r="100"
+              fill={circleColor}
+              // transform-origin: 145px 150px;
+            />
+          </Svg>
+        </Animated.View>
+        <Animated.View style={circleStyle}>
+          <Svg viewBox={'0 0 300 300'}>
+            <Circle
+              cx="150"
+              cy="155"
+              r="100"
+              fill={circleColor}
+              // transform-origin: 145px 150px;
+            />
+          </Svg>
+        </Animated.View>
+        <Animated.View style={circleStyle}>
+          <Svg viewBox={'0 0 300 300'}>
+            <Circle
+              cx="145"
+              cy="150"
+              r="100"
+              fill={circleColor}
+              // transform-origin: 145px 150px;
+            />
+          </Svg>
+        </Animated.View>
+        <Animated.View style={circleStyle}>
+          <Svg viewBox={'0 0 300 300'}>
+            <Circle
+              cx="155"
+              cy="150"
+              r="100"
+              fill={circleColor}
+              // transform-origin: 145px 150px;
+            />
+          </Svg>
+        </Animated.View>
+      </View>
     );
   }
 }
